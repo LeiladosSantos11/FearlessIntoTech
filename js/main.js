d21700b07e60c756a488ab69f356b2669ea42a8f
Leila dos Santos <28732563+LeiladosSantos11@users.noreply.github.com>
2021-03-03 22:58:56 +0000

    Add files via upload
    Final changes to JS and CSS files.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-
+/*navBar*/
 function myFunction() {
-  var x = document.getElementById("myLinks");
+  const x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
 }
+
+/*TypeJs on home page */
+const typed = new Typed('#typed',{
+  strings: [
+    '^1000',
+    '<span>M</span>OTIVATING',
+    '<span>I</span>MPORWERING',
+  "<span>D</span>RIVING CHANGES"
+  ],
+  backSpeed: 40,
+  typeSpeed: 40,
+  smartBackspace: true
+});
+
+
+
